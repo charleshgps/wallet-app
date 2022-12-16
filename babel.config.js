33a8cdd8c6d03371e0ba0f83fcad4502330020fa
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-16 18:32:08 -0300

    step 1

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo']
-  };
+module.exports = function (api) {
+    api.cache(true);
+    return {
+        presets: ['babel-preset-expo'],
+        plugins: [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                alias: {
+                    '@assets': './src/assets',
+                    '@components': './src/components',
+                    '@config': './src/config',
+                    '@hooks': './src/hooks',
+                    '@routes': './src/routes',
+                    '@screens': './src/screens',
+                    '@services': './src/services',
+                    '@styles': './src/styles',
+                    '@utils': './src/utils',
+                },
+            },
+        ],
+    };
 };
---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App: React.FC = () => {
+    return (
+        <View style={{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}>
+            <Text> Wallet App </Text>
+        </View>
+    )
+}
+
+export default App;

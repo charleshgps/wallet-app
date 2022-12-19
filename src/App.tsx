8113125bd9c567eab189b5ab2bc0e06a59b76b53
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2022-12-19 20:53:28 -0300

    fontes e tema adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,44 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { View, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components/native';
+
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+} from "@expo-google-fonts/poppins";
+import { DMSans_400Regular } from "@expo-google-fonts/dm-sans";
+import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+import COLORS from '../src/styles/theme'
 
 const App: React.FC = () => {
+    const [fontsloaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold,
+        Poppins_800ExtraBold,
+        DMSans_400Regular,
+        DMSerifDisplay_400Regular
+    })
+    if (!fontsloaded) {
+        return <AppLoading />
+    }
+
     return (
-        <View style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-        }}>
-            <Text> Wallet App </Text>
-        </View>
+        <ThemeProvider theme={COLORS}>
+            <View style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+            }}>
+                <Text> Wallet App </Text>
+            </View>
+        </ThemeProvider>
     )
 }
 

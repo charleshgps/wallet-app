ca7fcf84eb788e0c3bbaa28de40712e88c37ec53
Charles Silva <42080053+charleshgps@users.noreply.github.com>
2023-02-22 21:22:47 -0300

    ,

--- babel.config.js
+++ babel.config.js
@@ -2,24 +2,22 @@ module.exports = function (api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins:
-            [
-                'module-resolver',
-                {
-                    root: ['./src'],
-                    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-                    alias: {
-                        '@assets': './src/assets',
-                        '@components': './src/components',
-                        '@config': './src/config',
-                        '@hooks': './src/hooks',
-                        '@routes': './src/routes',
-                        '@screens': './src/screens',
-                        '@services': './src/services',
-                        '@styles': './src/styles',
-                        '@utils': './src/utils',
-                    },
-                },
-            ],
+        plugins: ['module-resolver', {
+            root: ['./src'],
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+            alias: {
+                '@assets': './src/assets',
+                '@components': './src/components',
+                '@config': './src/config',
+                '@hooks': './src/hooks',
+                '@routes': './src/routes',
+                '@screens': './src/screens',
+                '@services': './src/services',
+                '@styles': './src/styles',
+                '@utils': './src/utils'
+            },
+        },
+            'react-native-reanimated/plugin',
+        ],
     };
 };
